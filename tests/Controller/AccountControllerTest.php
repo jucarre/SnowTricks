6abd9c40d5ed43e2883754918a8217b22ea1d4ed
Julien Carré <juliencarre06@gmail.com>
2019-05-13 16:34:12 +0200

    Update AccountControllerTest.php

--- tests/Controller/AccountControllerTest.php
+++ tests/Controller/AccountControllerTest.php
@@ -6,15 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AccountControllerTest extends WebTestCase
 {
-    public function testShowHome()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-    }
-
     public function testShowDashboard()
     {
         $client = static::createClient();
